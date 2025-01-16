8f21a25480156041b3f04704a24a1a238df82899
Thanakrit Pongtanawannagon <thanakrit03b@gmail.com>
2025-01-16 22:35:01 +0700

    test-maybe finish

--- test-game-engine.py
+++ test-game-engine.py
@@ -13,13 +13,13 @@ class Ball:
         self.speed = min(self.speed + 1, max_speed) 
     
     def stop_moving(self,):
-        self.speed = max(self.speed - 0.5, 0)
+        self.speed = max(self.speed - 0.3, 0)
     
     def turn_left(self):
-        self.direction = (self.direction + 10) % 360
+        self.direction = (self.direction + 5) % 360
     
     def turn_right(self):
-        self.direction = (self.direction - 10) % 360
+        self.direction = (self.direction - 5) % 360
     
     def move(self):
         angle = math.radians(self.direction)
@@ -32,13 +32,23 @@ class Ball:
     def get_status(self):
         print(f"Ball position: {self.x}, {self.y} ball speed: {self.speed} ball direction: {self.direction}")
 
+class Food:
+    def __init__(self, x, y):
+        self.x = random.randint(0 + 5, 800 - 5)
+        self.y = random.randint(0 + 5, 600 - 5)
+
+    def respawn(self):
+        self.x = random.randint(0 + 5, 800 - 5)      # +5,-5 -> I don't want it to spawn on the edge
+        self.y = random.randint(0 + 5, 600 - 5)
+
 def test_ball():
     ball = Ball(400, 300)
+    food = Food(400, 300)
     frame = 0
     
     try:
         while True:
-            input_player1 = random.choice([['w', 'a'], ['w', 'd']])
+            input_player1 = random.choice([[], ['w'], ['a'], ['d'], ['w', 'a'], ['w', 'd']])
             input_player2 = random.choice([[], ['w'], ['a'], ['d'], ['w', 'a'], ['w', 'd']])
             if 'w' in input_player1:
                 ball.move_forward()
@@ -49,15 +59,17 @@ def test_ball():
                 ball.turn_left()
             if 'd' in input_player1:
                 ball.turn_right()
-            # if keyboard.is_pressed('q'):
-            #     break
-                
+           
             ball.move()
             
             frame += 1
+            print("\nFrame", frame)
+            print(ball.get_status())
+            print("food position: ", food.x, food.y)
             if frame % 10 == 0:
-                print("\nFrame", frame)
-                print(ball.get_status())
+                food.respawn()
+                print("food respawned")
+
             
             time.sleep(0.1)
             
